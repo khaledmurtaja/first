4f0768f2aa514d290ff4a4f916a2c14bf1b43bbb
zero1 <zero1@mbpalkhasbzero1>
2021-02-15 23:45:38 +0200

    hgff

---
+++ src/javaapplication169/JavaApplication169.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication169;
+
+
+
+/**
+ *
+ * @author zero1
+ */
+public class JavaApplication169 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static double cal(int x,int v){
+      double in=Math.pow(x, v);
+      return in;
+        
+    }
+    public static void main(String[] args) {
+double s=JavaApplication169.cal(667, 937);
+        System.out.println(s);
+   
+    }
+    
+}
---
+++ src/javaapplication169/outerclass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication169;
+
+/**
+ *
+ * @author zero1
+ */
+public class outerclass {
+
+    static class innerclass{
+        int b=5;
+    }
+    
+}
